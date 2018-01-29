b4d192485e905be7d8f4e8742d5997fc5300b79f
Schools <stephen.schools@caci.com>
2018-01-29 10:10:49 -0500

    initial commit

---
+++ src/main/java/com/example/arraybody/ArraybodyApplication.java
@@ -0,0 +1,12 @@
+package com.example.arraybody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArraybodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArraybodyApplication.class, args);
+	}
+}
